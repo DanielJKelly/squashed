94ea7c26a3974a42f0c4d9f8ffbb73914a0873fe
Daniel Kelly <djk8552@gmail.com>
2018-02-01 22:35:31 -0500

    change test db name back to codeop

--- test/database-spec/test-db.js
+++ test/database-spec/test-db.js
@@ -11,7 +11,7 @@ describe('codeOp database', () => {
   beforeEach((done) => {
     dbConnection = mysql.createConnection({
       user: 'root',
-      database: 'test',
+      database: 'codeop',
     });
     dbConnection.connect((err) => {
       if (err) {
